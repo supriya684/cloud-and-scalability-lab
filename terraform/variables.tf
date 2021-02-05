6d47a1f00f70c8443c7eb93e7584d191242e2444
supriya.supriya <supriya.supriya@cevt.se>
2021-02-05 15:17:58 +0100

    add app service

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,5 +19,5 @@ variable "location" {
 variable "name" {
   description = "The name to use for the different parts of the deployment"
   type        = string
-  default     = "webapp_supriya"
+  default     = "webapp-supriya2"
 }
